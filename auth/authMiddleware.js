bd638f980653459fe5d558054139c641665ebcb7
Franco Perrone <fperronerey@gmail.com>
2026-05-06 09:34:22 -0300

    primer utilizacion de servicio

---
+++ auth/authMiddleware.js
@@ -0,0 +1,21 @@
+// generamos el middle
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+
+
+const authMiddleware = (req, res, next) => {
+    const token = req.headers.authorization?.split(' ')[1];
+    if (!token) {
+        return res.status(401).json({ error: 'Acceso denegado' });
+    }
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        return res.status(401).json({ error: 'Token inválido' });
+    }
+};
+
+export default authMiddleware;
---
+++ products/routes.js
@@ -0,0 +1,26 @@
+// GET /api/products devuelve todos los productos.
+
+// GET /api/products/:id devuelve el producto con el ID indicado.
+
+// POST /api/products/create recibe en el cuerpo (body) de la petición la información sobre el nuevo producto para ser guardado en el servicio de datos en la nube.
+
+// DELETE /api/products/:id elimina el producto con el ID indicado.
+
+import express from 'express';
+import { getProductos, getProductoById, createProducto, deleteProducto } from './controller.js';
+
+const router = express.Router();
+
+// Ruta para obtener todos los productos
+router.get('/products', getProductos);
+
+// Ruta para obtener un producto por ID
+router.get('/products/:id', getProductoById);
+
+// Ruta para crear un nuevo producto
+router.post('/products/create', createProducto);
+
+// Ruta para eliminar un producto
+router.delete('/products/:id', deleteProducto);
+
+export default router;
